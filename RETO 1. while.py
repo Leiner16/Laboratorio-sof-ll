d20cfa24633a0f02561d5f642e413c6a43c54625
Leiner <leiner.mendoza@unillanos.edu.co>
2022-07-29 07:23:55 -0500

    Se agrego una segunda parte de codigo a RETO 1. while.py

--- RETO 1. while.py
+++ RETO 1. while.py
@@ -34,3 +34,30 @@ while faltan == "S":
     else:
         break
     contador = contador + subtotal
+
+#segunda parte del reto
+
+def modificar_materia(pensum, semestre, materia, nombre, creditos):
+    # ACÁ INICIA LA FUNCIÓN modificar_materia (En este espacio debes 
+    # poner el código necesario para implementar esta funcionalidad)
+    if semestre not in range(1, len(pensum) + 1):
+        mensaje = "Ingrese un semestre válido"
+    elif len(pensum[semestre - 1]) == 0:
+        mensaje = "El semestre no tiene materias"
+    else:
+        if materia not in pensum[semestre - 1]:
+            mensaje = "La materia no existe"
+        else:
+            pensum[semestre-1][materia]["créditos" ]=creditos
+            pensum[semestre-1][materia]["nombre" ]= nombre
+            mensaje="Modificada con éxito"
+    # ACÁ TERMINA LA FUNCIÓN modificar_materia
+    # NO MODIFIQUES LA SIGUIENTE LÍNEA
+    return mensaje
+d=pensum=[
+    {"0123":{"nombre": "intro a la ing", "créditos": 2 },
+    "4567":{"nombre": "inglés", "créditos": 1}},
+    {},
+    {}],
+modificar_materia(d,"0123","nombre: intro a la ing y créditos 2,","hhhh",11111)
+
